12e2c88889cb19ecf84f83b4007624802c0a1b91
Bernard Adhitya <bernard.adhitya@gmail.com>
2020-04-10 10:07:05 +0700

    [Bernard] Web scrape specific url in IMDb

---
+++ index.js
@@ -0,0 +1,30 @@
+const puppeteer = require('puppeteer');
+
+(async () => {
+
+    let movieUrl = 'https://www.imdb.com/title/tt7286456/?ref_=hm_fanfav_tt_5_pd_fp1';
+
+    let browser = await puppeteer.launch();
+    let page = await browser.newPage();
+
+    await page.goto(movieUrl, { waitUntil: 'networkidle2'});
+
+    let data = await page.evaluate(() => {
+        let title = document.querySelector('div[class="title_wrapper"] > h1').innerText;
+        let rating = document.querySelector('span[itemprop="ratingValue"]').innerText;
+        let ratingCount = document.querySelector('span[itemprop="ratingCount"]').innerText;
+
+        return {
+            title,
+            rating,
+            ratingCount
+        }
+    })
+
+    console.log(data);
+
+    debugger;
+
+    await browser.close();
+    
+})();
